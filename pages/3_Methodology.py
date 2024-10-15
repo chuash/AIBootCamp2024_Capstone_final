b504f132f25f176dea083ae6ff5c326d998add68
Seow How <seowhow@gmail.com>
2024-10-15 22:49:11 +0800

    changes to methodology page

--- pages/3_Methodology.py
+++ pages/3_Methodology.py
@@ -6,6 +6,16 @@ st.set_page_config(layout="wide", page_title="My Streamlit App")
 
 st.title("Methodology")
 st.write("- A comprehensive explanation of the data flows and implementation details.")
-st.write("- A flowchart illustrating the process flow for each of the use cases in the application.")
-st.image("./images/Flow_chart1.jpg, caption= flow chart showing process flow for use case 1")
-st.image("./images/Flow_chart2.jpg, caption= flow chart showing process flow for use case 2")
+st.write(
+    "- A flowchart illustrating the process flow for each of the use cases in the application."
+)
+st.image(
+    "./images/Flow_chart1.jpg",
+    caption="flow chart showing process flow for use case 1",
+    width=1024,
+)
+st.image(
+    "./images/Flow_chart2.jpg",
+    caption="flow chart showing process flow for use case 2",
+    width=1024,
+)
