fa2d2e012c71a8615a3b57fa08341642aeaf8294
Seow How <seowhow@gmail.com>
2024-10-25 01:01:38 +0800

    Edited Methodology.py

--- logics/rag_preretrieval.py
+++ logics/rag_preretrieval.py
@@ -8,13 +8,6 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 # initialise embedding model to be used for RAG
 embeddings_model = OpenAIEmbeddings(model="text-embedding-3-small")
 
-# Step 1: Loading data from external sources (2 from HDB and 1 from CPF)
-HDBurl1 = (
-    "https://www.hdb.gov.sg/cs/infoweb/e-resale/resale-purchase-of-an-hdb-resale-flat"
-)
-HDBurl2 = "https://www.hdb.gov.sg/residential/buying-a-flat/understanding-your-eligibility-and-housing-loan-options/flat-and-grant-eligibility/couples-and-families/cpf-housing-grants-for-resale-flats-families"
-CPFurl1 = "https://www.cpf.gov.sg/member/infohub/educational-resources/hdb-option-fee-and-housing-expenses-you-should-know"
-
 
 def extract_clean(url):
     """Function to scrape content from provided url, extract the page metadata and clean up the text"""
@@ -42,79 +35,78 @@ def extract_clean(url):
     return metadata, cleaned_text
 
 
-# First HDB url
-metadata_hdb1, cleaned_text_hdb1 = extract_clean(HDBurl1)
-# Remove excess newlines, specific to this HDB website
-cleaned_text_hdb1 = re.sub(r"\n{3,}", "\n\n", cleaned_text_hdb1)
-# Filter for relevant content
-cleaned_text_hdb1 = cleaned_text_hdb1.split("APPLICABLE TO BOTH SELLER AND BUYER\n\n")[
-    1
-].split("\n\nNOTE:")[0]
-# Create document object with clean text and subsetted metadata
-doc_hdb1 = Document(
-    page_content=cleaned_text_hdb1,
-    metadata={key: metadata_hdb1[key] for key in ["source", "title", "description"]},
-)
+if __name__ == "__main__":
+    # Step 1: Loading data from external sources (2 from HDB and 1 from CPF)
 
-# First CPF url
-metadata_cpf, cleaned_text_cpf = extract_clean(CPFurl1)
-# Remove excess newlines, specific to this CPF website
-cleaned_text_cpf = re.sub(r"\n{2}", "\n", cleaned_text_cpf)
-cleaned_text_cpf = re.sub(r"\n{3,}", "\n\n", cleaned_text_cpf)
-# Filter for relevant information
-cleaned_text_cpf = cleaned_text_cpf.split("CPF Board\n\n")[1].split(
-    "\n\nInformation accurate as of date of publication"
-)[0]
-# Create document object with clean text and subsetted metadata
-doc_cpf = Document(
-    page_content=cleaned_text_cpf,
-    metadata={key: metadata_cpf[key] for key in ["source", "title", "description"]},
-)
+    HDBurl1 = "https://www.hdb.gov.sg/cs/infoweb/e-resale/resale-purchase-of-an-hdb-resale-flat"
+    HDBurl2 = "https://www.hdb.gov.sg/residential/buying-a-flat/understanding-your-eligibility-and-housing-loan-options/flat-and-grant-eligibility/couples-and-families/cpf-housing-grants-for-resale-flats-families"
+    CPFurl1 = "https://www.cpf.gov.sg/member/infohub/educational-resources/hdb-option-fee-and-housing-expenses-you-should-know"
 
-# Second HDB url
-metadata_hdb2, cleaned_text_hdb2 = extract_clean(HDBurl2)
-# Remove excess newlines, specific to this HDB website
-cleaned_text_hdb2 = re.sub(r"\n{3,}", "\n\n", cleaned_text_hdb2)
-# Filter for relevant information
-cleaned_text_hdb2 = cleaned_text_hdb2.split(
-    "CPF Housing Grants for Resale Flats (Families)\n\nCPF Housing Grants for Resale Flats (Families)\n\n"
-)[1].split("\n\nNEXT STEPS")[0]
-# Create document object with clean text and subsetted metadata
-doc_hdb2 = Document(
-    page_content=cleaned_text_hdb2,
-    metadata={key: metadata_hdb2[key] for key in ["source", "title", "description"]},
-)
+    # First HDB url
+    metadata_hdb1, cleaned_text_hdb1 = extract_clean(HDBurl1)
+    # Remove excess newlines, specific to this HDB website
+    cleaned_text_hdb1 = re.sub(r"\n{3,}", "\n\n", cleaned_text_hdb1)
+    # Filter for relevant content
+    cleaned_text_hdb1 = cleaned_text_hdb1.split("APPLICABLE TO BOTH SELLER AND BUYER\n\n")[1].split("\n\nNOTE:")[0]
+    # Create document object with clean text and subsetted metadata
+    doc_hdb1 = Document(
+        page_content=cleaned_text_hdb1,
+        metadata={key: metadata_hdb1[key] for key in ["source", "title", "description"]},
+    )
 
-# Combining the docs into a list of loaded documents
-docs = [doc_hdb1, doc_cpf, doc_hdb2]
+    # First CPF url
+    metadata_cpf, cleaned_text_cpf = extract_clean(CPFurl1)
+    # Remove excess newlines, specific to this CPF website
+    cleaned_text_cpf = re.sub(r"\n{2}", "\n", cleaned_text_cpf)
+    cleaned_text_cpf = re.sub(r"\n{3,}", "\n\n", cleaned_text_cpf)
+    # Filter for relevant information
+    cleaned_text_cpf = cleaned_text_cpf.split("CPF Board\n\n")[1].split("\n\nInformation accurate as of date of publication")[0]
+    # Create document object with clean text and subsetted metadata
+    doc_cpf = Document(
+        page_content=cleaned_text_cpf,
+        metadata={key: metadata_cpf[key] for key in ["source", "title", "description"]})
 
+    # Second HDB url
+    metadata_hdb2, cleaned_text_hdb2 = extract_clean(HDBurl2)
+    # Remove excess newlines, specific to this HDB website
+    cleaned_text_hdb2 = re.sub(r"\n{3,}", "\n\n", cleaned_text_hdb2)
+    # Filter for relevant information
+    cleaned_text_hdb2 = cleaned_text_hdb2.split("CPF Housing Grants for Resale Flats (Families)\n\nCPF Housing Grants for Resale Flats (Families)\n\n")[1].split("\n\nNEXT STEPS")[0]
+    # Create document object with clean text and subsetted metadata
+    doc_hdb2 = Document(
+        page_content=cleaned_text_hdb2,
+        metadata={key: metadata_hdb2[key] for key in ["source", "title", "description"]},
+    )
 
-# Step 2: split the loaded documents into appropriate chunk length
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200,  # set at 20% of chunk size
-    add_start_index=True,
-    keep_separator=False,
-    strip_whitespace=True,
-    # length_function= llm.count_tokens,
-    # is_separator_regex=True, # if this is true, it can recognise "\. " as period separator, if False, will see this as \. separator
-)
+    # Combining the docs into a list of loaded documents
+    docs = [doc_hdb1, doc_cpf, doc_hdb2]
 
-splitted_documents = text_splitter.split_documents(docs)
+    # Step 2: split the loaded documents into appropriate chunk length
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,  # set at 20% of chunk size
+        add_start_index=True,
+        keep_separator=False,
+        strip_whitespace=True,
+        # length_function= llm.count_tokens,
+        # is_separator_regex=True, # if this is true, it can recognise "\. " as period separator, if False, will see this as \. separator
+    )
 
-# Step 3: Embedding and storage of chunked documents
-idlist = [
-    str(doc.metadata["source"]) + " - " + str(doc.metadata["start_index"])
-    for doc in splitted_documents
-]
-vector_store = Chroma.from_documents(
-    collection_name="HDBResale",
-    documents=splitted_documents,
-    embedding=embeddings_model,
-    ids=idlist,
-    collection_metadata={"hnsw:space": "cosine"},
-    persist_directory="../data/chroma_langchain_db",  # Where to save data locally
-)
+    splitted_documents = text_splitter.split_documents(docs)
 
-print(f"Number of docs in chromadb: {vector_store._collection.count()}")
-print(f"First doc: {vector_store._collection.peek(limit=1)}")
+    # Step 3: Embedding and storage of chunked documents
+    idlist = [
+        str(doc.metadata["source"]) + " - " + str(doc.metadata["start_index"])
+        for doc in splitted_documents
+    ]
+    vector_store = Chroma.from_documents(
+        collection_name="HDBResale",
+        documents=splitted_documents,
+        embedding=embeddings_model,
+        ids=idlist,
+        collection_metadata={"hnsw:space": "cosine"},
+        persist_directory="../data/chroma_langchain_db",  # Where to save data locally
+    )
+
+    print(f"Number of docs in chromadb: {vector_store._collection.count()}")
+    print(f"First doc: {vector_store._collection.peek(limit=1)}")
--- pages/3_Methodology.py
+++ pages/3_Methodology.py
@@ -5,17 +5,67 @@ st.set_page_config(layout="wide", page_title="My Streamlit App")
 # endregion <--------- Streamlit App Configuration --------->
 
 st.title("Methodology")
-st.write("- A comprehensive explanation of the data flows and implementation details.")
-st.write(
-    "- A flowchart illustrating the process flow for each of the use cases in the application."
-)
+st.write("""In **About Us**, an overview of the features for the three tools supporting the three use cases is presented. Here, the implementation details
+         and data flows for each tool will be explained, accompanied by flow charts illustrating the process flow behind each tool.""")
+
+st.subheader("ResaleStats")
 st.image(
-    "./pages/images/Flow_chart1.JPG",
-    caption="flow chart showing process flow for use case 1",
+    "./pages/images/ResaleStats.drawio.png",
+    caption="Flow chart showing overall process flow for ResaleStats",
     width=1024,
 )
+st.write("""The 5 downloaded csv files were preprocesed before visualisation. The following details the data preprocessing steps. After preprocessing, visualisations
+         , including filters, are created using standard streamlit functions.""")
+data_transformation = """
+**Data preprocessing and transformation**
+1) *HDB Resale Price Index (1Q2009 = 100), Quarterly*
+    - Read csv into pandas dataframe and filter for time period ('quarter') to start from 2004-Q1.
+    - Keep the 'quarter' and 'index' fields, and store the dataframe as csv in the data folder.
+2) *Median Resale Prices for Registered Applications by Town and Flat Type*
+    - Read csv into pandas dataframe and filter for time period ('quarter') to start from 2020-Q1 (past 5 years).
+    - Replace 'na' and '-' values in the price field with None.
+    - Select the fields 'quarter', 'town', 'flat_type' and 'price', and store the dataframe as csv in the data folder.
+3) *Resale flat prices based on registration date from Jan-2017 onwards*
+    - Read csv into pandas dataframe and filter for time period ('month') to start from 2023-10 (past 12 months).
+    - Remove redundant field - 'remaining_lease', and store the dataframe as csv in the data folder.
+4) *CEA Salespersons’ Property Transaction Records (residential)*, *CEA Salesperson Information*
+    - Read both csvs into pandas dataframes.
+    - For the dataframe from the first csv:
+        - Convert the 'transaction_date' field to python datetime format and filter to start from 2023-09-01
+        - Remove records that are not associated with any salesperson.
+        - Given the focus on HDB flat resale activity, remove other transaction activities by filtering for 'property_type' is HDB and 
+          'transaction_type' is resale.
+    - Left-join this processed dataframe with the dataframe from the second csv by the common key -> salesperson registration number, so
+      as to get the real estate company that the salesperson is employed under
+    - Select the relevant fields and store the joint dataframe as csv in the data folder.\n   
+(For specific codes, refer to https://github.com/chuash/AIBootCamp2024_Capstone_final/blob/main/data_prep.py)
+"""
+st.write(data_transformation)
+st.write("""In addition to interactive data analysis, users are also able to query data using words or natural language. The following details the implementation
+         steps for the chat with data agent""")
 st.image(
-    "./pages/images/Flow_chart2.JPG",
-    caption="flow chart showing process flow for use case 2",
+    "./pages/images/ResaleStatsChatAgent.drawio.png",
+    caption="Flow chart showing process flow for chat with data agent",
+    width=512,
+)
+agent = """
+**Chat with data agent**
+- Under the hood, the 'create_pandas_dataframe_agent' from *langchain_experimental.agents.agent_toolkits* is used, which in turn utilises OpenAI's 'gpt-4o-mini' model.
+- When user inputs query, an OpenAI API call is made to ask gpt-4o-mini, suitably prompted with system message and few shot learning examples, to assess for
+  potential prompt injection and malicious instructions. 
+- If assessment is negative, then another OpenAI API call will be made to ask gpt-4o-mini, again suitably prompted with system message and few shot learning examples,to
+  assess if the query is related to the underlying dataset. To help the LLM better assess, the dataset metadata is included in the system message to let the LLM understand
+  the data it has access to. As added precaution against prompt injection, XML-like tags are used in both system and user messages.
+- If assessment is positive, i.e. query is related to underlying dataset, then the 'create_pandas_dataframe_agent' is initialised, which will then access python repl tool
+  to execute script on the underlying dataset. Queried results from the dataset are then passed to LLM to generate user response.
+  
+(For specific codes, refer to https://github.com/chuash/AIBootCamp2024_Capstone_final/blob/main/logics/agent.py)
+"""
+st.write(agent)
+
+st.subheader("ResaleSearch")
+st.image(
+    "./pages/images/RAG.png",
+    caption="Flow chart showing overall process flow for ResaleSearch",
     width=1024,
 )
